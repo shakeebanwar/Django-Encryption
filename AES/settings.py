413e7e120cfbcac1fe01fcdc5cb222818589a2da
shakeeb <shakeebanwar250@gmail.com>
2020-11-07 15:17:10 +0500

    done

--- AES/settings.py
+++ AES/settings.py
@@ -121,9 +121,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+#SEND LOCALLY
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hussainfazal175@gmail.com'
 EMAIL_HOST_PASSWORD = 'fazalhus'
+
+
+
+# SEND Online
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'vmi359795.contaboserver.net'
+# EMAIL_USE_SSL = True
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = 'haider@hnh6.xyz'
+# EMAIL_HOST_PASSWORD = 'alwaysbehappy'
